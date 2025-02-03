4114cd0283f167b7ff8ba25f4b8cb259cb859ffa
Emma Ådahl Görling <143135641+EmmaGorling@users.noreply.github.com>
2025-02-03 14:14:24 +0100

    Skapat grund för formulär och installerat yup

--- src/components/TodoForm.tsx
+++ src/components/TodoForm.tsx
@@ -1,8 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const TodoForm = () => {
+
+    // Interface
+    interface FormData {
+        title: string, 
+        description: string,
+        status: string
+    }
+
+    // State
+    const [formData, setFormData] = useState<FormData>({
+        title: "",
+        description: "",
+        status: "Ej påbörjad"
+    });
+
+
     return (
-        <div>TodoForm</div>
+        <div>
+            <form action="">
+                <label htmlFor="title">Titel</label>
+                <input 
+                    type="text" 
+                    name='title' 
+                    id='title' 
+                    value={formData.title} 
+                    onChange={(event) => {setFormData({...formData, title: event.target.value})}}
+                />
+                <br />
+                <label htmlFor="description">Beskrivning</label>
+                <textarea 
+                    name="description" 
+                    id="description" 
+                    value={formData.description}
+                    onChange={(event) => {setFormData({...formData, description: event.target.value})}}>
+                </textarea>
+                <br />
+                <button type='submit'>Lägg till uppgift</button>
+            </form>
+        </div>
     )
 }
 
