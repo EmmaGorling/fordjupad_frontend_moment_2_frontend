6d02a7ea10af1c21095fa43f540afdb25ce4b3eb
Emma Ådahl Görling <143135641+EmmaGorling@users.noreply.github.com>
2025-02-06 10:47:43 +0100

    Fix för build-kommando

--- src/components/buttons/DeleteButton.tsx
+++ src/components/buttons/DeleteButton.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import { useState } from 'react'
 import './DeleteButton.css';
 
 const DeleteButton = ({ id, getTodos } : { id: string, getTodos: Function }) => {
